e2dbc60a7f7fdfa1b360fd388483cbd2f009b61d
Sheena Gonzalez <shngonzalez@gmail.com>
2024-01-03 13:14:50 -0800

    Added solution to 1

---
+++ 01 - Easy/1-two-sum.js
@@ -0,0 +1,58 @@
+/** 
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+  You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+  You can return the answer in any order.
+
+  
+
+  Example 1:
+
+  Input: nums = [2,7,11,15], target = 9
+  Output: [0,1]
+  Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+  Example 2:
+
+  Input: nums = [3,2,4], target = 6
+  Output: [1,2]
+  Example 3:
+
+  Input: nums = [3,3], target = 6
+  Output: [0,1]
+  
+
+  Constraints:
+
+  2 <= nums.length <= 104
+  -109 <= nums[i] <= 109
+  -109 <= target <= 109
+  Only one valid answer exists.
+  
+
+  Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+*/
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+  // Create hashmap
+  const targetDifferenceIndex = {};
+
+  // Fill hashmap with target difference as key, and its index as value
+  for (let i = 0; i < nums.length; i++) {
+      targetDifferenceIndex[target - nums[i]] = i;
+  }
+
+  // Check if target difference value exists in nums array
+  // If exists, return both indices
+  for (let j = 0; j < nums.length; j++) {
+      let index2 = targetDifferenceIndex[nums[j]];
+      if (index2) {
+          return [j, index2];
+      }
+  }
+};
